3fdfb0feac68ccb611f0c317315bf23e4bbf5e91
Yash Chaudhari <120511697+yashc2003@users.noreply.github.com>
2024-01-23 15:13:42 +0000

    add navbar and footer

---
+++ src/Componant/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './../../Componant/Footer/Footer.css'
+function Footer () {
+    return (
+      <div>
+        <h3>Footer</h3>
+        </div>
+      
+    )
+  }
+  
+  export default Footer
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./../Home/Home.css"
+import Navbar from "./../../Componant/Navbar/Navbar"
+import Footer from "./../../Componant/Footer/Footer"
 function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Home Page</h1>
+      <Footer/>
       </div>
     
   )
