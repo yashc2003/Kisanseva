bf873b1ff58076e41c72fdac838d4a89f0738c5c
Yashc782003 <157696280+Yashc782003@users.noreply.github.com>
2024-01-25 12:22:17 +0000

    add image galery in home page

---
+++ src/Componant/Image-card/Card.js
@@ -0,0 +1,220 @@
+import React from "react";
+import "./../../Componant/Image-card/Card.css"
+function Card() {
+    return (
+      <div>
+              <div class="contener">
+           <div>
+              <div class="card">
+                <img class="card-img"src="https://media.gettyimages.com/id/157484373/photo/sugar-cane-from-below.jpg?s=612x612&w=0&k=20&c=dklesMkf5mWwM17DwoN36eEEFZ1AwzYfNtaZ3YoO7Uo=" alt=".."/>
+               <div class="card-text">
+               <h2 class="h2">SUGERCANE</h2>
+                <button>Read More</button>
+               </div>
+              </div>
+           </div>
+           <div>
+            <div class="card">
+              <img class="card-img"src="https://media.gettyimages.com/id/521177840/photo/grapes-growing-in-napa-valley.jpg?s=612x612&w=0&k=20&c=_Bg7Vi_BPq587rfjWVibd12QdQ5FB8YNI0MlTYvXgsE=" alt=".."/>
+             <div class="card-text">
+             <h2 class="h2">   GRAPE</h2>
+              <button>Read More</button>
+             </div>
+            </div>
+         </div>
+         <div>
+            <div class="card">
+              <img class="card-img"src="https://media.gettyimages.com/id/1178874080/photo/coffee-berries-close-up-arabica-coffee-berries-with-agriculturist-hands.jpg?s=612x612&w=0&k=20&c=F0cgjYIbVUqJGXAyH2j0V1P8QNo7jFOjIIC05S8zHwI=" alt=".."/>
+             <div class="card-text">
+             <h2 class="h2">COFFEE</h2>
+              <button>Read More</button>
+             </div>
+            </div>
+         </div>
+         <div>
+            <div class="card">
+              <img class="card-img"src="https://media.gettyimages.com/id/165051017/photo/cotton-boll.jpg?s=612x612&w=0&k=20&c=-sAv0qGB2Vs3I6FkyU7MNZ6CaCbBSOqhH1_bCXdkHYI=" alt=".."/>
+             <div class="card-text">
+             <h2 class="h2">COTTON</h2>
+              <button>Read More</button>
+             </div>
+            </div>
+         </div>
+         <div>
+            <div class="card">
+              <img class="card-img"src="https://media.gettyimages.com/id/479629438/photo/wheat.jpg?s=612x612&w=0&k=20&c=fJpTBCLVasl_dbMgkofYeZuxBbhpxZ6uM9IxgMzEiAo=" alt=".."/>
+             <div class="card-text">
+             <h2 class="h2">WHEAT</h2>
+              <button>Read More</button>
+             </div>
+            </div>
+         </div>
+         <div>
+          <div class="card">
+            <img class="card-img"src="https://media.gettyimages.com/id/1348971759/photo/close-up-of-wheat-growing-on-field-tirunelveli-tamil-nadu-india.jpg?s=612x612&w=0&k=20&c=iV4vpYlSMTo47ALfuGDcMQ00l01YSNtYRgZ8b-5zKYM=" alt=".."/>
+           <div class="card-text">
+           <h2 class="h2">PADDY</h2>
+            <button>Read More</button>
+           </div>
+          </div>
+       </div>
+       <div>
+          <div class="card">
+            <img class="card-img"src="https://media.gettyimages.com/id/1267272011/photo/fresh-corn-cob-on-dark-background.jpg?s=612x612&w=0&k=20&c=LSaKjPiuc3SDm18GU09x1rzwJ0VEFTE9FRUTT-X86rY=" alt=".."/>
+           <div class="card-text">
+           <h2 class="h2">MAIZE</h2>
+            <button>Read More</button>
+           </div>
+          </div>
+       </div>
+       <div>
+          <div class="card">
+            <img class="card-img"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT12X8pzvyu-nGwo-Ig8RgkV1YruHcq3mPMow&usqp=CAU" alt=".."/>
+           <div class="card-text">
+           <h2 class="h2">GROUNDNUT</h2>
+            <button>Read More</button>
+           </div>
+          </div>
+       </div>
+       <div>
+        <div class="card">
+          <img class="card-img"src="https://media.gettyimages.com/id/976851866/photo/wooden-basket-full-of-ripe-oranges-in-orange-grove.jpg?s=612x612&w=0&k=20&c=qhDOY3b9ID-rUZeUqj4K1neojihheihjXeBv8bo60Po=" alt=".."/>
+         <div class="card-text">
+         <h2 class="h2">ORANGE</h2>
+          <button>Read More</button>
+         </div>
+        </div>
+     </div>
+     <div>
+      <div class="card">
+        <img class="card-img"src="https://media.gettyimages.com/id/163120954/photo/citrus.jpg?s=612x612&w=0&k=20&c=SxLFnbjtURBAs2kt7zsTy0Ot_H8VbTaAG6k5wbkeFY4=" alt=".."/>
+       <div class="card-text">
+       <h2 class="h2">CITRUS</h2>
+        <button>Read More</button>
+       </div>
+      </div>
+   </div>
+   <div>
+      <div class="card">
+        <img class="card-img"src="https://media.gettyimages.com/id/170486397/photo/mangoes-are-a-popular-summer-fruit-in-bangladesh-june-3-2007.jpg?s=612x612&w=0&k=20&c=3denrPHX_T5T82En00NhCLA3PodsykrIOGfH3NDfpfg=" alt=".."/>
+       <div class="card-text">
+       <h2 class="h2">MANGO</h2>
+        <button>Read More</button>
+       </div>
+      </div>
+   </div>
+   <div>
+      <div class="card">
+        <img class="card-img"src="https://media.gettyimages.com/id/1185374212/photo/pomegranates-cultivation-in-south-apulia.jpg?s=612x612&w=0&k=20&c=89cvT8TCxjeAxKG75zIEe7XeuV60gXzxgv9DNfIJ9oo=" alt=".."/>
+       <div class="card-text">
+       <h2 class="h2">POMEGRANATE</h2>
+        <button>Read More</button>
+       </div>
+      </div>
+   </div>
+   <div>
+    <div class="card">
+      <img class="card-img"src="https://media.gettyimages.com/id/1065370286/photo/bunch-of-bananas.jpg?s=612x612&w=0&k=20&c=M-k3c6su8VdxvoE1SdAJT2Z2mOscO8B3eXOC1DQqVHE=" alt=".."/>
+     <div class="card-text">
+     <h2 class="h2">BANANA</h2>
+      <button>Read More</button>
+     </div>
+    </div>
+ </div>
+ <div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/521289762/photo/papaya-fruit.jpg?s=612x612&w=0&k=20&c=90PKGTlPHcJ99w9-_oOOe_X_PtrnUu95yeLbQmAvXSw=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">PAPAYA</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/1399390336/photo/high-angle-view-of-watermelon-on-wooden-table.jpg?s=612x612&w=0&k=20&c=cC52fNjA9v8IOLEDPHEY3nlqPodBbeKft0Hhxg9K-Og=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">WATERMELON</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/1372722029/photo/cuernavaca-colombia-cacao-fruits-are-seen-growing-on-cacao-trees-on-a-traditional-cacao-farm.jpg?s=612x612&w=0&k=20&c=glj1bqLUKY4MTl8_g8VJnff_Q9FFXUw-QSOlIvlFRE8=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">COCOA</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+    <div class="card">
+      <img class="card-img"src="https://media.gettyimages.com/id/98890395/photo/stack-of-red-onions-in-market-place.jpg?s=612x612&w=0&k=20&c=JnJAZANDnA7hXvvH2atwhXytaukeXgJY-iaChMpsgbM=" alt=".."/>
+     <div class="card-text">
+     <h2 class="h2">ONION</h2>
+      <button>Read More</button>
+     </div>
+    </div>
+ </div>
+ <div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/98026003/photo/tomatoes.jpg?s=612x612&w=0&k=20&c=d5wExaxj68PSVNeWho3KF9PROq1T0v0WhFEv4WVyyKw=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">TOMATO</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/91383674/photo/fresh-potatoes.jpg?s=612x612&w=0&k=20&c=EtkoWY0Wr88g8Qauv6zwk5Hi8e8Vh2tkV3t_bE5faO8=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">POTATO</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/155419207/photo/close-up-of-cauliflower-growing-in-field.jpg?s=612x612&w=0&k=20&c=y90xUHmWBxLpKa9CixkUzCrWv1G71KPrAlrYOG5W9aQ=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">CAULIFLOWER</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+  <div class="card">
+    <img class="card-img"src="https://media.gettyimages.com/id/1290200732/photo/close-up-fresh-chilli-pepper-in-garden-concept-organic-chillies-vegetable-planting-in-farm.jpg?s=612x612&w=0&k=20&c=lt-pGwd7N9nX6VzTYUZp7TNTANEyBfr_oGHjT2ZrORU=" alt=".."/>
+   <div class="card-text">
+   <h2 class="h2">CHILLI</h2>
+    <button>Read More</button>
+   </div>
+  </div>
+</div>
+<div>
+<div class="card">
+  <img class="card-img"src="https://media.gettyimages.com/id/597062235/photo/fresh-curcuma-and-powder-on-wood.jpg?s=612x612&w=0&k=20&c=_9BYuNdnNzHV-fglmkN23e6Hr1ghf3LLAhnzCJ2tpsc=" alt=".."/>
+ <div class="card-text">
+ <h2 class="h2">TURMERIC</h2>
+  <button>Read More</button>
+ </div>
+</div>
+</div>
+<div>
+<div class="card">
+  <img class="card-img"src="https://media.gettyimages.com/id/128112512/photo/pile-of-ginger-root-on-green-placemat.jpg?s=612x612&w=0&k=20&c=r7sOP1xBe-eo3hgaWl1VhaUv1b4ypM7GAqYGvl_qXOo=" alt=".."/>
+ <div class="card-text">
+ <h2 class="h2">GINGER</h2>
+  <button>Read More</button>
+ </div>
+</div>
+</div>
+</div>
+</div>
+      
+    )
+  }
+  
+  export default Card
--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import "./../Home/Home.css"
 import Navbar from "./../../Componant/Navbar/Navbar"
 import Footer from "./../../Componant/Footer/Footer"
+import Imagecard from "./../../Componant/Image-card/Card"
 function Home() {
   return (
     <div>
       <Navbar/>
       <h1>Home Page</h1>
+      <Imagecard/>
       <Footer/>
       </div>
     
