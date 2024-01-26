268afeced44859e93be15122c1cc595b8dd5a37e
Yashc782003 <157696280+Yashc782003@users.noreply.github.com>
2024-01-26 09:25:42 +0000

    create banner and image card

---
+++ src/Componant/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./../../Componant/Banner/Banner.css"
+function Banner() {
+    return (
+
+        <div class="banner">
+        <img class="banner-img "src="https://d3nn873nee648n.cloudfront.net/900x600/20374/300-PT1052818.jpg"alt=".."/>
+        <div class="banner-text">
+            <h1 class="banner-text-h1">CROPS</h1>
+            <h3 class="banner-text-h3">Better Harvest Better Tomorrow</h3>
+        </div>
+       </div>
+
+)
+}
+
+export default Banner
---
+++ src/Componant/Banner/Banner2.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./../../Componant/Banner/Banner2.css"
+function Banner2() {
+    return (
+    <div class="banner2">
+   <div class="banner2-text">
+       <h1 class="banner2-text-h1">Subscribe to get information and latest news.</h1>
+       <input class="banner2-text-input" type="location" placeholder="Enter email address" aria-label="email"/>
+   </div>
+   <button class="banner2-button">SUBSCRIBE</button>
+   <div class="banner2-logo">
+       <img class="banner2-img" src="https://o.remove.bg/uploads/d1a38541-fddd-4725-9d98-94342d136b22/images__1.png" alt=""/>
+   </div>
+  </div>
+
+)
+}
+
+export default Banner2
