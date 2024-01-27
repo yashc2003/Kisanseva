a4af1ae6986370f2dbb148015634c95c5c066738
Pushpakparkhe <pushpakparkhe2@gmail.com>
2024-01-27 12:45:34 +0530

    navbar changes dropdown added

--- src/Componant/Navbar/Navbar.js
+++ src/Componant/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./../../Componant/Navbar/Navbar.css"
 import search from "./../../Componant/assets/search-w.png"
 import toogleicon from "./../../Componant/assets/day.png"
-import logo from "./../../Componant/assets/kpng.png"
+import logo from "./../../Componant/assets/k2.png"
 
 
 
@@ -11,17 +11,51 @@ function Navbar() {
       <div className="navbar">
         <img src= {logo} alt="" className="logo"/>
       <ul>
-        <li>crop</li>
-        <li>product & services </li>
-        <li>Deparmental help</li>
-        <li>contact us</li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Product & Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Organic And Bio Product</a></li>
+            <li><a class="dropdown-item" href="#">Crop Specification</a></li>
+            <li><a class="dropdown-item" href="#">Other Product</a></li>
+          </ul>
+        </li> <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Crops
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Cottan</a></li>
+            <li><a class="dropdown-item" href="#">Wheat</a></li>
+            <li><a class="dropdown-item" href="#">Soyabin</a></li>
+          </ul>
+        </li> <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            About Us
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Company Owerview</a></li>
+            <li><a class="dropdown-item" href="#">History</a></li>
+            <li><a class="dropdown-item" href="#">Milestone</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contact 
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Watsapp</a></li>
+            <li><a class="dropdown-item" href="#">Mail</a></li>
+            <li><a class="dropdown-item" href="#">contact</a></li>
+          </ul>
+        </li>
+       
       </ul>
       <div className="searchbox">
      <input type="text" placeholder="search"/>
      <img src={search} alt="" className="search"/>
       </div>
       
-      <img src={toogleicon} alt="" className="toogle-icon"/>
         </div>
       
     )
