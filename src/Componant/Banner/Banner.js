27b812f7f1ea04c4ce4eba909832448f681d09d8
Yashc782003 <157696280+Yashc782003@users.noreply.github.com>
2024-02-01 15:53:57 +0000

    update image slider

--- src/Componant/Banner/Banner.js
+++ src/Componant/Banner/Banner.js
@@ -6,13 +6,40 @@ function Banner() {
         <div id="carouselExampleAutoplaying" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active ">
-            <img src="https://www.bestagrolife.com/img/Pioneering-The-Future-With-Innovative-Research.jpg" class="d-block w-100 " alt="..."/>
+          <div class="card-slider">
+              <img class="card-slider-img"src="https://www.bestagrolife.com/img/Pioneering-The-Future-With-Innovative-Research.jpg" alt=".."/>
+            <div class="card-text1">
+               <h1 class="h11">Pioneering The Future
+                      With Innovative Research</h1>
+                  <p class="paragraph">We are an innovative and forward-thinking company that seeks to play a crucial role in modernizing 
+                     agriculture while also prioritizing the well-being of people and the planet.</p>
+            </div>
+          </div>
           </div>
           <div class="carousel-item">
-            <img src="https://www.bestagrolife.com/img/iStock-1061166708-min.webp" class="d-block w-100 " alt="..."/>
+          <div>
+            <div class="card-slider">
+               <img class="card-slider-img"src="https://www.bestagrolife.com/img/iStock-1061166708-min.webp" alt=".."/>
+              <div class="card-text1">
+               <h1 class="h11">Farmer Friendly Solutions</h1>
+                <p class="paragraph">We strive to optimize opportunities essential to each farmer in their support by offering cost-effective yield-enhancing products to make the most of every acre.
+                   Our focus is on newer chemistry to facilitate an edge to drive synergy in the way agriculture is done. 
+                </p>
+              </div>
+             </div>
+           </div>
           </div>
           <div class="carousel-item">
-            <img src="https://www.bestagrolife.com/img/iStock-543212762-min.webp" class="d-block w-100 " alt="..."/>
+          <div>
+            <div class="card-slider">
+              <img class="card-slider-img"src="https://www.bestagrolife.com/img/iStock-543212762-min.webp" alt=".."/>
+             <div class="card-text1">
+               <h1 class="h11">Let's Come Together
+                               For A Greener Future!</h1>
+                  <p class="paragraph">We welcome your inquiries about the company and its products.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
