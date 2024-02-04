e97deb6a41fcf80b5fa6c56431fafad07ad03751
Pushpakparkhe <pushpakparkhe2@gmail.com>
2024-02-04 11:50:55 +0530

    micronutrient page added

--- src/view/Product & Services/Micronutrients.js
+++ src/view/Product & Services/Micronutrients.js
@@ -7,7 +7,79 @@ import Footer from './../../Componant/Footer/Footer';
     return(
         <div>
             <Navbar/>
-            <h1>Micronutrients</h1>
+            
+            <div class="bannerhed">
+    <img class="sugercaneimg" src="https://img.freepik.com/premium-photo/business-growth-progress-success-concept-invest-trading-illustration_10221-11576.jpg" alt=".."/>
+    <div class="text-containt">
+        <h1 class="heading">MULTI MICRONUTRIENTS AND MIX MICRONUTRIENT FERTILIZERS</h1>
+        <p class="sub-heading">better harvest better tomorrow</p>
+    </div>
+    </div>
+            <p className="p"> Plant nutrients needed in micro levels are called as micronutrients in Agriculture. Mineral elements comprising of Zinc (Zn), 
+                Copper (Cu), Manganese (Mn), Iron (Fe), Boron (B), Molybdenum (Mo) are the essential multi micronutrients needed for plant growth and development.</p>
+             <p className="p">Multi Micronutrients are crucial for plant growth and play an important role in balancing crop nutrition. Micronutrients, 
+                though needed in minute amounts play an important role in most enzymatic reactions as they act as co-factors for enzymes.</p>
+                <p className="p">Micronutrients help the major elements to perform their functions. If they are not present, the major elements will not be able to perform their duties even if you supply them.
+                     Here is the list of micronutrients that are popularly known to be as best micronutrients for plants in India. </p>
+                     <p className="p">The common roles are overall plant growth, photosynthesis, chlorophyll formation, cell wall development resistance to plant disease, nitrogen fixation, 
+                        colour of the fruits, sugar transport, fruit textures, the turgidity of the plant,
+                         and osmotic balances. Water Water-soluble micronutrients are meant to supply sufficient plant nutrients to aquatic plants, irrespective of their size. </p>
+                         <p className="p">There are many more benefits that can be easily attained from micronutrient fertilizer products.
+                          All you have to know is how to use micronutrients in agriculture. </p>
+
+                          <div class="card w-75 mb-40">
+  <div class="card-body">
+    <h5 class="card-title">Chelated Micronutrients</h5>
+    <p className="p2">Micronutrients such as Fe, Mn, Zn, and Cu are easily oxidized or precipitated in soil, and their utilization is, therefore, not efficient. Chelated fertilisers have been developed to increase micronutrient utilization efficiency. When in chelated form, the metal isn’t precipitated by phosphates, carbonates and other soil and fertiliser components.
+     In this free, chelated form, it remains mobile and unfixed. It is able to move freely through the soil for uptake by the plant root system.</p>
+   <p className="p2">The word chelate is derived from the Greek word chelé, which refers to a lobster’s claw. Hence, chelate refers to the pincer-like way in which a metal nutrient ion like Zinc, Manganese, Iron and Copper is encircled by the larger organic molecule (the claw), usually called a ligand or chelator. Some commonly used chelating agents are EDTA, DTPA, EDDHA, amino acids, organic acids etc.
+     The effectiveness and efficiency of a particular chelated fertiliser depends on the pH of the plant growth medium.</p>
+  </div>
+</div>
+<br></br> <br></br> <br></br> <br></br>
+
+<div class="card w-75 mb-40">
+  <div class="card-body">
+    <h5 class="card-title">Customised Micronutrients</h5>
+    <p className="p2">Micronutrient fertilisers include inorganic salts, chelated fertilisers and multi micronutrient formulations formulated as state specific grades as provided by the different state governments. Customized micronutrient fertilisers are special micronutrient blends which are made for specific condition of soil and recommended for special crops and hence in this way promote site specific nutrient management.
+     These are soil-crop-climate based that lead to more uptake of nutrients and less loss of nutrient maximising fertiliser use efficiency in a cost effective manner.  </p>
+  
+  </div>
+</div>
+<br></br> <br></br> <br></br> <br></br>
+
+
+<div class="card w-75 mb-40">
+  <div class="card-body">
+    <h5 class="card-title">Zinc Fertilisers</h5>
+    <p className="p2">Zinc (Zn) is an essential micronutrient that has significant role in basic plant metabolic processes and enhances the growth, yield and quality by stimulating chlorophyll production, photosynthetic activity, nutrient uptake and protein biosynthesis.
+     Zinc fertilisers are available both in sulphate and chelated forms and can be applied either through soil or as a foliar spray.</p>
+
+  </div>
+</div>
+<br></br> <br></br> <br></br> <br></br>
+
+
+<div class="card w-75 mb-40">
+  <div class="card-body">
+    <h5 class="card-title">Boron Micronutrients</h5>
+    <p className="p2">Boron (B) is a micronutrient critical to the growth and health of all crops. It is a component of plant cell walls and reproductive structures and plays a key role in a diverse range of plant functions including cell wall formation and stability, maintenance of structural and functional integrity of biological membranes, movement of sugar or energy into growing parts of plants, and pollination and seed set. Adequate B is also required for effective nitrogen fixation and nodulation in legume crops. 
+    The two common boron containing fertilisers are Borax and Boric acid and can be given through soil or as a foliar spray at critical development stages.</p>
+  
+  </div>
+</div>
+<br></br> <br></br> <br></br> <br></br>
+
+
+<div class="card w-75 mb-40">
+  <div class="card-body">
+    <h5 class="card-title"> Iron Fertilisers</h5>
+    <p className="p2">Iron (Fe) plays a significant role in various physiological and biochemical pathways in plants. It serves as a component of many vital enzymes such as cytochromes, it is involved in the synthesis of chlorophyll and is essential for the maintenance of chloroplast structure and function. 
+    Iron fertilisers are available in sulphated and chelated forms and can be given either through drip or as a foliar spray.</p>
+   
+  </div>
+</div>
+<div></div>
             <Footer/>
         </div>
     )
