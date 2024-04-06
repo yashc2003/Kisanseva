1d5ccc230fdf36198a893fa7a5510b0fd645da91
taihan khane <taihankhan86@gmail.com>
2024-04-06 20:20:37 +0530

    preloader functionality  added

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -5,12 +5,15 @@ import Banner from "./../../Componant/Banner/Banner"
 import Banner2 from "./../../Componant/Banner/Banner2"
 import Footer from "./../../Componant/Footer/Footer"
 import Imagecard from "./../../Componant/Image-card/Card"
+import Preloader from '../../Componant/preloader/preloader'
+
 function Home() {
   console.log(process.env.REACT_APP_APPWRITE_URL)
   return (
     <div>
       <Navbar/>
       <Banner/>
+      <Preloader/>
       <Imagecard/>
       <Banner2/>
       <Footer/>
