8a6115d70d971034b6fe32bb388d6867553dd94b
Pushpakparkhe <pushpakparkhe2@gmail.com>
2024-04-17 11:33:11 +0530

    video

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -9,6 +9,7 @@ import Footer from "./../../Componant/Footer/Footer"
 import Imagecard from "./../../Componant/Image-card/Card"
 import Preloader from '../../Componant/preloader/preloader'
 import WeatherApp from './../../Componant/Weather app/WeatherApp'
+import Video from './../../Componant/assets/1.mp4'
 function Home() {
   console.log(process.env.REACT_APP_APPWRITE_URL)
   return (
@@ -18,6 +19,13 @@ function Home() {
       <Preloader/>
       <WeatherApp/>
       <Imagecard/>
+      <div>
+      <div class="VidClass">
+    <video src={Video} type="video/mp4" autoplay loop muted plays-inline class="video col-lg-8 col-sm-8 col-md-8 col-xs-8 mt-5 mb-5 ms-5" >
+      
+    </video>
+  </div>
+  </div>
       <Cardslider/>
       <Cardslider2/>
       <Banner2/>
