296db07abef70cf3bdf153d95b94a8e84aaa27b6
Yashc782003 <157696280+Yashc782003@users.noreply.github.com>
2024-05-08 11:58:25 +0000

    dropdouwn error solve

--- src/Componant/Navbar/Navbar.js
+++ src/Componant/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../../Componant/Navbar/Navbar.css"
-import { Link } from 'react-router-dom';
+
 import Translater from "./../../Componant/Google translater/TanslaterComponent"
 import Navbarimag from "./../../Componant/assets/kpng1.png";
 
